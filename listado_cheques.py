4a428e413c40208d6b161513d877cfb9131af469
lichaa824 <lisandrorubi@gmail.com>
2022-06-30 12:44:52 -0300

    Now dates are properly displayed on console log

--- listado_cheques.py
+++ listado_cheques.py
@@ -1,48 +1,62 @@
 import csv
 import sys
-from datetime import date , datetime
+from datetime import date, datetime
 from pprint import pprint
+from collections import Counter
 
-
-cheques = [];
-estados = ["PENDIENTE","APROBADO","RECHAZADO"];
 
 def filtradoPorFecha(rango):
     argumento_fecha = rango.split(":")
-    fechaUno = list(map(int,argumento_fecha[0].split("-")));
-    fechaDos = list(map(int,argumento_fecha[1].split("-")));
-    fechaDesde = date(fechaUno[2],fechaUno[1],fechaUno[0]);
-    fechaHasta = date(fechaDos[2],fechaDos[1],fechaDos[0]);
+    fechaUno = list(map(int, argumento_fecha[0].split("-")))
+    fechaDos = list(map(int, argumento_fecha[1].split("-")))
+    fechaDesde = date(fechaUno[2], fechaUno[1], fechaUno[0])
+    fechaHasta = date(fechaDos[2], fechaDos[1], fechaDos[0])
     for cheque in cheques[:]:
         if date.fromtimestamp(int(cheque["FechaPago"])) > fechaHasta or date.fromtimestamp(int(cheque["FechaOrigen"])) < fechaDesde:
-            cheques.remove(cheque);
+            cheques.remove(cheque)
+
 
 def salida(tipo):
     if tipo.upper() == "PANTALLA":
+        for cheque in cheques:
+            cheque["FechaPago"] = str(
+                date.fromtimestamp(int(cheque["FechaPago"])))
+            cheque["FechaOrigen"] = str(
+                date.fromtimestamp(int(cheque["FechaOrigen"])))
         for cheque in cheques:
             pprint(cheque)
             print("\n")
     else:
         dt = datetime.now()
-        with open(f'{sys.argv[2]}_{int(round(datetime.timestamp(dt)))}','w') as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames= ['NroCheque','FechaOrigen', 'FechaPago', 'NumeroCuentaDestino', 'NumeroCuentaOrigen', 'Valor'], extrasaction='ignore')
+        with open(f'{sys.argv[2]}_{int(round(datetime.timestamp(dt)))}', 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=[
+                'FechaOrigen', 'FechaPago', 'Valor', 'NumeroCuentaDestino', 'NumeroCuentaOrigen'], extrasaction='ignore')
             writer.writeheader()
             writer.writerows(cheques)
 
 
+cheques = []
+estados = ["PENDIENTE", "APROBADO", "RECHAZADO"]
+
 # Lee y crea dictionario con el csv
 CSV_FILE = open(sys.argv[1], "r")
-csv_dict = csv.DictReader(CSV_FILE);
-
-
+csv_dict = csv.DictReader(CSV_FILE)
 
 # Filtra cheques del CSV que contengan el DNI especifico
 
 for cheque in csv_dict:
     if cheque["DNI"] == sys.argv[2]:
-        cheques.append(cheque);
+        cheques.append(cheque)
 
-CSV_FILE.close();
+CSV_FILE.close()
+
+# Verifica que no haya numero de cheque duplicado por DNI
+
+counter = Counter((d['NroCheque']) for d in cheques)
+for value in counter:
+    if int(counter[value]) > 1:
+        raise Exception("Numero de cheque duplicado!")
+
 
 # Elimina los cheques del array que no sean del Tipo especificado
 
@@ -64,5 +78,3 @@ if len(sys.argv) > 5:
 
 
 salida(sys.argv[3])
-    
-        
